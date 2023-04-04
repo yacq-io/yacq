144ff927c95ac38245b71e24488f977e40381528
Ben Swierzy <swierzy@uni-bonn.de>
2023-04-04 19:01:04 +0200

    Removed Vue sample files

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p>Work in progress...</p>
   </main>
 </template>
